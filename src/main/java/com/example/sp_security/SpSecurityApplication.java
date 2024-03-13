140a3539f5a6ac5a6b95e8ac5134067b970a9f46
dogyuwon <ds257873@naver.com>
2024-03-14 01:17:38 +0900

    Initial commit

---
+++ src/main/java/com/example/sp_security/SpSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.sp_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpSecurityApplication.class, args);
+    }
+
+}
